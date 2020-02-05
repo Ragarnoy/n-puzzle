b13f5d33931c6a42154a7a04db0a89015d1e9219
Tiago LERNOULD <tiagolernould@hotmail.com>
2020-02-05 20:27:13 +0100

    Add manning, foundations of manhattan

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,20 @@ mod node;
 use clap::{Arg, App};
 use std::{path::Path, fs};
 
-
 fn main() {
-        let matches = App::new("N-Puzzle")
-                    .version(crate_version!())
-                    .author(crate_authors!())
-                    .about(crate_description!())
-                    .arg(Arg::with_name("input")
-                        .required(true)
-                        .help("<file.txt> input"))
-                    .get_matches();
+    //     let matches = App::new("N-Puzzle")
+    //                 .version(crate_version!())
+    //                 .author(crate_authors!())
+    //                 .about(crate_description!())
+    //                 .arg(Arg::with_name("input")
+    //                     .required(true)
+    //                     .help("<file.txt> input"))
+    //                 .get_matches();
 
-    let file = Path::new(matches.value_of("input").unwrap());
+    // let file = Path::new(matches.value_of("input").unwrap());
 
-    let content = fs::read_to_string(file).unwrap();
-    println!("{}", content);
+    // let content = fs::read_to_string(file).unwrap();
+    // println!("{}", content);
 
     // TODO Parser here
 }
