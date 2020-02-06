bf4d8566195d8005d3f20766dad2107f73c98e40
Tiago LERNOULD <tiagolernould@hotmail.com>
2020-02-06 16:53:23 +0100

    Impl Ord for State

--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,6 @@
+use std::cmp::{Ordering, Ord};
+
+#[derive(Eq, PartialEq, PartialOrd)]
 pub struct State
 {
     h:u16,
@@ -6,3 +9,33 @@ pub struct State
 }
 
 // TODO Generate h from grid method that executes heuristic funtion
+
+impl Ord for State
+{
+    fn cmp(&self, other: &Self) -> Ordering
+    {
+        if self.f > other.f
+        {
+            Ordering::Greater
+        }
+        else if self.f < other.f
+        {
+            Ordering::Less
+        }
+        else
+        {
+            if (self.h > other.h) || (self.h == other.h && self.g > other.g)
+            {
+                Ordering::Greater
+            }
+            else if (self.h < other.h) || (self.h == other.h && self.g < other.g)
+            {
+                Ordering::Less
+            }
+            else
+            {
+                Ordering::Equal
+            }
+        }
+    }
+}
